98cd3ba4a691b07a5e08d812570986ea21c0f276
bruno <brunoliracz@gmail.com>
2022-10-08 12:18:43 -0300

    init project

---
+++ src/main/kotlin/com/br/gerenciamentovotacao/GerenciamentoVotacaoApplication.kt
@@ -0,0 +1,11 @@
+package com.br.gerenciamentovotacao
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GerenciamentoVotacaoApplication
+
+fun main(args: Array<String>) {
+	runApplication<GerenciamentoVotacaoApplication>(*args)
+}
---
+++ src/test/kotlin/com/br/gerenciamentovotacao/GerenciamentoVotacaoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.br.gerenciamentovotacao
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GerenciamentoVotacaoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
